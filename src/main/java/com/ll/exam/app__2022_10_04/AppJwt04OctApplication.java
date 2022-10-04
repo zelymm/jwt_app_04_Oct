28c020d9887404ad53ce3101694ec606a945b4a9
zelymm <dkdlwkrz@naver.com>
2022-10-04 15:16:32 +0900

    initial commit

---
+++ src/main/java/com/ll/exam/app__2022_10_04/AppJwt04OctApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_10_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJwt04OctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJwt04OctApplication.class, args);
+    }
+
+}
